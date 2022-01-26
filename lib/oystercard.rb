5c039e75b0fad023f87c5bc36b7f3fbf88a353c9
sapna tavrawala <sapnatavrawala@outlook.com>
2022-01-26 15:52:04 +0000

    Adding journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-  attr_reader :balance, :entry_station, :exit_station
+  attr_reader :balance, :entry_station, :journey_history, :exit_station
 
   MAX_LIMIT = 90       
   MIN_LIMIT = 1
@@ -7,25 +7,27 @@ class Oystercard
   def initialize
     @balance = 0
     @journey_history = []
+    
   end
-  
+
+
   def top_up(amount)
     @balance += amount
     fail "Overlimit: Limit is £#{MAX_LIMIT}" if @balance > MAX_LIMIT
   end
 
-  private def deduct(amount)
-    @balance -= amount  
-  end
-
   def touch_in(entry_station)
     fail "No balance: Minimum £#{MIN_LIMIT}" if @balance < MIN_LIMIT
-    @entry_station = entry_station
-    
+    @journey_history << {:entry => entry_station}
   end
 
-  def touch_out(entry_station)
+  def touch_out(exit_station)
     deduct(MIN_LIMIT)
-    @entry_station = nil
+    @journey_history.last[:exit] = exit_station
+  end
+
+
+  private def deduct(amount)
+    @balance -= amount  
   end
 end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,50 +2,45 @@ require 'oystercard'
 
 describe Oystercard do
   let(:entry_station) { double :station}
+  let(:exit_station) { double :station}
   
 
   it "can show balance" do
-    oystercard = Oystercard.new
-   expect(oystercard.balance).to eq 0
+   expect(subject.balance).to eq 0
   end
 
   it "top up balance" do
-    oystercard = Oystercard.new
-    oystercard.top_up(10)
-    expect(oystercard.balance).to eq 10 
+    subject.top_up(10)
+    expect(subject.balance).to eq 10 
   end
 
   it "can go overlimit" do
-    oystercard = Oystercard.new
-    expect{oystercard.top_up(100)}.to raise_error "Overlimit: Limit is £90"
-  end
-  # it  'can shows if its in journey' do
-  #   oystercard = Oystercard.new
-  #   expect(oystercard.in_journey?).to be false
-  # end
-
-  it "can touch-in" do
-    oystercard = Oystercard.new
-    oystercard.top_up(20)
-    
-    expect(oystercard.touch_in(entry_station)).to eq entry_station
-  end
-
-  it "can touch-out" do
-    oystercard = Oystercard.new
-    oystercard.top_up(20)
-    expect(oystercard.touch_out(entry_station)).to eq nil
+    expect{subject.top_up(100)}.to raise_error "Overlimit: Limit is £90"
   end
 
   it "should have minimum balance" do
-    oystercard = Oystercard.new
-    expect{oystercard.touch_in(entry_station)}.to raise_error "No balance: Minimum £1"
+    expect{subject.touch_in(entry_station)}.to raise_error "No balance: Minimum £1"
   end
 
   it "correct amount deducted from my card" do
-    oystercard = Oystercard.new
-    oystercard.top_up(20)
-    oystercard.touch_in(entry_station)
-    expect {oystercard.touch_out(entry_station)}.to change{oystercard.balance}.by(-1)
+    subject.top_up(20)
+    subject.touch_in(entry_station)
+    expect {subject.touch_out(exit_station)}.to change{subject.balance}.by(-1)
   end
+
+  it "checks that card has empty list of journey" do
+    expect(subject.journey_history).to be_empty
+  end
+
+  it "checks that touching in and out creates journey" do
+    subject.top_up(20)
+    subject.touch_in(entry_station) # {:entry => entry_station}
+    subject.touch_out(exit_station) # [{:entry => entry_station}, {:exit=> exit_station}]
+    expect(subject.journey_history).to include({:entry => entry_station, :exit => exit_station})
+  end
+
+
 end
+
+
+
